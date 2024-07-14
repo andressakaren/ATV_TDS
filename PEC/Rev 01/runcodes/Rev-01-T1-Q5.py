c2ed148486eea15a701689e69eb45911ef6d5a05
andressakaren <160805915+andressakaren@users.noreply.github.com>
2024-07-14 14:39:43 -0300

    change q5

--- PEC/Rev 01/runcodes/Rev-01-T1-Q5.py
+++ PEC/Rev 01/runcodes/Rev-01-T1-Q5.py
@@ -1,6 +1,6 @@
 def main():
     custoFabrica = float(input(''))
-    # custoCarroNovo = custoFabrica + porcentagemDist (28%) + imposto(45%) * custoFabriga
+    # custoCarroNovo = custoFabrica + (porcentagemDist (28%) * custoFabrica) + (imposto(45%) * custoFabrica)
     custoCarroNovo = custoFabrica + (0.28 * custoFabrica) + (0.45 * custoFabrica)
     print(f'R$ {custoCarroNovo:.2f}')
 if __name__ == '__main__':
