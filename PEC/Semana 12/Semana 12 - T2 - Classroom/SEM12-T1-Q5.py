280de35ee75dc55befdec1bd521858746215e853
andressakaren <160805915+andressakaren@users.noreply.github.com>
2024-08-26 10:52:43 -0300

    feat: Semana 12 - T2

--- PEC/Semana 12/Semana 12 - T2 - Classroom/SEM12-T1-Q5.py
+++ PEC/Semana 12/Semana 12 - T2 - Classroom/SEM12-T1-Q5.py
@@ -1,4 +1,32 @@
+# funçao da qst 4
+def eh_primo(num):   
+    cont = 0
+    i = 1  
+    
+    while i < (num + 1):
+        divisao = num % i
+        if divisao == 0:
+            cont += 1
+        i += 1          
+    if cont == 2: 
+        return True
+    else:
+        return False    
+        
 def main():
-    ...
+    num_X = int(input('Digite o primeiro número: '))
+    num_Y = int(input('Digite o segundo número: '))
+    
+    if num_X > num_Y:
+        # inverter para o segundo ser sempre o maior 
+        num_X = num_Y
+        num_Y = num_X
+
+    i = num_X
+    while i <= num_Y:
+        if eh_primo(i):
+            print(i)
+        i += 1      
+    
 if __name__ == "__main__":
     main()
--- PEC/Semana 12/Semana 12 - T2 - Runcodes/SEM12-T1-Q5.py
+++ PEC/Semana 12/Semana 12 - T2 - Runcodes/SEM12-T1-Q5.py
@@ -1,37 +1,32 @@
-# DISGRAAAAAAAAÇA
-
 # funçao da qst 4
-# def eh_primo():
-#     numDigitado = int(input())
-#     cont = 0
-#     i = 1
+def eh_primo(num):   
+    cont = 0
+    i = 1  
     
-#     while i < (numDigitado + 1):
-#         divisao = numDigitado % i
-#         if divisao == 0:
-#             cont += 1
-#         i += 1    
-        
-#     if cont == 2: 
-#         print('True')
-#     else:
-#         print('False')    
+    while i < (num + 1):
+        divisao = num % i
+        if divisao == 0:
+            cont += 1
+        i += 1          
+    if cont == 2: 
+        return True
+    else:
+        return False    
         
 def main():
-    numDigitado_X = int(input())
-    numDigitado_Y = int(input())
+    num_X = int(input())
+    num_Y = int(input())
     
-    # if numDigitado_X > numDigitado_Y:
-    #     # inverter para o segundo ser sempre o maior 
-    #     numDigitado_X = numDigitado_Y
-    #     numDigitado_Y = numDigitado_X
-        
-    # i = numDigitado_X
-    # count = 0
-    
-    # while i < (numDigitado_Y + 1):
-    #     count += 1
-        
+    if num_X > num_Y:
+        # inverter para o segundo ser sempre o maior 
+        num_X = num_Y
+        num_Y = num_X
+
+    i = num_X
+    while i <= num_Y:
+        if eh_primo(i):
+            print(i)
+        i += 1      
     
 if __name__ == "__main__":
     main()
