14a40592a631aedbb5c47f07e5c71d96210eb8c2
andressakaren <160805915+andressakaren@users.noreply.github.com>
2024-08-29 22:05:15 -0300

    feat: Semana 13 - T2

--- PEC/Semana 13/Semana 13 - T2 - Classroom/SEM13-T2-Q5.py
+++ PEC/Semana 13/Semana 13 - T2 - Classroom/SEM13-T2-Q5.py
@@ -1,18 +1,20 @@
 def ler_valores(n):
     lista = []
     for i in range(n):
-        valor = input(f'Digite o {i + 1}° número: ').strip()
+        valor = input(f'Digite o {i + 1}° valor: ').strip()
         lista.append(valor)
     return lista
 
-def esta_ordenada(lista, lista_ordenada):
-    return lista == lista_ordenada
+def esta_ordenada(lista):
+    for i in range(len(lista) - 1):
+        if lista[i] > lista[i + 1]:
+            return False
+    return True # Só False se pelo menos um for menor que o próximo, True: valor PADRÃO.
 
 def main():
     n = int(input('Digite o valor da constante: '))
     lista = ler_valores(n)
-    lista_ordenada = sorted(lista)
-    conferencia = esta_ordenada(lista, lista_ordenada)
+    conferencia = esta_ordenada(lista)
     
     if conferencia:
         print('LISTA ORDENADA')
