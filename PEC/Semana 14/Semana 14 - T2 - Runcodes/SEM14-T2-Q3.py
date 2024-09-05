31b828a261556ddb5da471f34f8345480cf5ccfb
andressakaren <160805915+andressakaren@users.noreply.github.com>
2024-09-04 23:52:43 -0300

    feat: Semana 14 - T2

--- PEC/Semana 14/Semana 14 - T2 - Runcodes/SEM14-T2-Q3.py
+++ PEC/Semana 14/Semana 14 - T2 - Runcodes/SEM14-T2-Q3.py
@@ -1,5 +1,3 @@
-# incompleta
-
 def ler_itens(n):
     lista = []
     for i in range(n):
@@ -9,11 +7,16 @@ def ler_itens(n):
 
 def eliminar_repetidos(lista):
     lista_nova = []
-    ...
+    for i in range(len(lista)):
+        if lista[i] not in lista_nova:
+            lista_nova.append(lista[i])
+    return lista_nova     
     
 def main():
     qtdNum = 20
     lista = ler_itens(qtdNum)
+    lista_sem_repetidos = eliminar_repetidos(lista)
+    print(lista_sem_repetidos)
             
 if __name__ == '__main__':
     main()
