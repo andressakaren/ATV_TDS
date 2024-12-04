f59acd64f96d4506e0001b0c47b237de9a570493
Andressa Karen <160805915+andressakaren@users.noreply.github.com>
2024-12-04 17:30:59 -0300

    1 commit

--- PWF/pwf_exercicios_js_06/scripts.js
+++ PWF/pwf_exercicios_js_06/scripts.js
@@ -15,3 +15,19 @@ textoOrigem2.addEventListener('keyup', () => {
     let textoDestino2 = getById('textoDestino2');
     textoDestino2.value = textoOrigem2.value.toUpperCase();
 });
+
+// Questao 03
+let selecionarEmocao = getById('selecionarEmocao');
+let imagem1 = getById('imagem1');
+
+selecionarEmocao.addEventListener('change', () => {
+    let emocaoSelecionada = selecionarEmocao.value;
+    
+    if (emocaoSelecionada === 'alegria') {
+        imagem1.src = 'https://recreio.uol.com.br/media/uploads/disney/alegria_divertidamente.jpg';
+    } else if (emocaoSelecionada === 'tristeza') {
+        imagem1.src = 'https://recreio.uol.com.br/media/uploads/2024/06/tristeza.jpeg';
+    } else if (emocaoSelecionada === 'raiva') {
+        imagem1.src = 'https://recreio.uol.com.br/media/uploads/2024/06/raiva.jpg';
+    }
+});
