196c84cf216f0ce6ad63daf35f5cc22e66b38eed
andressakaren <160805915+andressakaren@users.noreply.github.com>
2024-12-11 08:41:27 -0300

    add def mudar_estacoes( )

--- POO/atv08_radio.py
+++ POO/atv08_radio.py
@@ -8,7 +8,7 @@ estacoes = {
 class RadioFM:
     def __init__(self, vol_max, estacoes):
         self.__volume_min = 0
-        self.__volume_max = vol_max # ALTERAR DEPOIS PARA O USUARIO INSERIR
+        self.__volume_max = vol_max
         self.__freq_min = 88
         self.__freq_max = 108
         self.__estacoes = estacoes
@@ -39,16 +39,16 @@ class RadioFM:
                 return self.habilitar_antena()
             else: 
                 while True:
-                    habilitar = str(input('Deseja habilitar a antena? S/N: ')).lower()
+                    habilitar = str(input('Deseja habilitar a antena? S/N: \n')).lower()
                     if habilitar == 's':                  
                         return self.habilitar_antena()
                     elif habilitar == 'n': 
-                        print('Se precisar habilitar a antena insira [3] - Habilitar antena')
+                        print('Se precisar habilitar a antena insira [3] - Habilitar antena\n')
                         break
                     else:
-                        print('Opção inválida')    
+                        print('Opção inválida\n')    
         else: 
-            print('Rádio já está ligada')      
+            print('Rádio já está ligada\n')      
                 
     def habilitar_antena(self):
         if self.__ligado:
@@ -60,22 +60,20 @@ class RadioFM:
                 #     print(i)               
                 self.__frequencia_atual = primeira_frequencia 
                 self.__estacao_atual = primeira_estacao
-                print()
-                print(f'Antena habilitada\nEstação: {self.__estacao_atual}\nFrequencia: {self.__frequencia_atual}')
             else:
-                print('Antena já está habilitada!') 
+                print('Antena já está habilitada!\n') 
         else:
             print('Não é possível habilitar a antena com a rádio desligada.')         
             while True:
-                ligar = str(input('Deseja ligar a radio? S/N: ')).lower()
+                ligar = str(input('Deseja ligar a radio? S/N: \n')).lower()
                 if ligar == 's':                  
                     return self.ligar()
                 elif ligar == 'n': 
                     print()
-                    print('Se precisar habilitar a antena insira [1] - Ligar rádio ')
+                    print('Se precisar habilitar a antena insira [1] - Ligar rádio\n')
                     break
                 else:
-                    print('Opção inválida')
+                    print('Opção inválida\n')
      
     # No método desligar, mudar o estado para False, além de atualizar os atributos: volume, frequencia_atual e estação_atual para: None
         
@@ -90,8 +88,6 @@ class RadioFM:
     # O método aumentar_volume deverá receber um atributo opcional com valor inicial igual a 1. Caso este valor seja passado na chamada do argumento, atualizar o volume com o valor do argumento (fazer a crítica para não ultrapassar os valores permitidos para o volume). Caso o argumento fique vazio na chamada, o atributo volume deverá ser incrementado de uma unidade. (Fazer a crítica para não ultrapassar o valor máximo permitido)
 
     def aumentar_volume(self, valor = 1):
-        # ARRUMAR ESSE MÉTODO DE VERIFICAR STATUS
-        # self.__verificar_status()
         if self.__ligado:
             if (self.__volume_min <= valor <= self.__volume_max):
                 if valor == 1:
@@ -108,7 +104,6 @@ class RadioFM:
             print('Requisição inválida. A radio está desligada!')
             
     def diminuir_volume(self, valor = 1):
-        # self.__verificar_status()
         if self.__ligado:
             if (self.__volume_min <= valor <= self.__volume_max):
                 if valor == 1:
@@ -127,6 +122,54 @@ class RadioFM:
     @property
     def volume(self):
         return self.__volume
+    
+    @property
+    def frequencia(self):
+        return self.__frequencia_atual
+    
+    @property
+    def estacao(self):
+        return self.__estacao_atual
+    
+    def mudar_frequencia(self, valor = 0):
+        # verificar se radio ligada 
+        # self.__verificar_status()
+        if self.__ligado:
+        
+            frequencias = list(self.__estacoes.keys())
+            nomes_estacoes = list(self.__estacoes.values())
+            
+            if valor == 0:
+                # Se a frequencia atual for igual ao último elemento do dicionário e/ou self.__frequencia_atual is None
+                if self.__frequencia_atual is None or self.__frequencia_atual == frequencias[-1]:
+                    # mudar frequencia atual para a primeira
+                    self.__frequencia_atual = frequencias[0]
+                else:
+                    indice_atual = frequencias.index(self.__frequencia_atual)
+                    self.__frequencia_atual = frequencias[indice_atual + 1]
+                    
+            elif valor == 1:
+                self.__frequencia_atual = frequencias[0]
+                self.__estacao_atual = nomes_estacoes[0]
+                
+            elif valor == 2:
+                self.__frequencia_atual = frequencias[1]
+                self.__estacao_atual = nomes_estacoes[1]
+                
+            elif valor == 3:
+                self.__frequencia_atual = frequencias[2]
+                self.__estacao_atual = nomes_estacoes[2]
+                
+            elif valor == 4:
+                self.__frequencia_atual = frequencias[3]
+                self.__estacao_atual = nomes_estacoes[3]
+                
+            else:
+                print('Opção inválida')
+        else: 
+            print('Requisição inválida. A radio está desligada!')    
+            
+        print(f"Frequência atual: {self.__frequencia_atual}\nEstação atual: {self.__estacao_atual}")           
     
     def __str__(self):
         return (
@@ -167,8 +210,9 @@ O que deseja fazer:
 [4] - Aumentar volume              
 [5] - Diminuir volume   
 [6] - Mudar estação 
-[7] - Descrição atual 
-[8] - Sair do programa          
+[7] - Frequencia e estação atual 
+[8] - Descrição atual 
+[9] - Sair do programa          
               ''')
         resposta = input('')
         print()
@@ -193,13 +237,31 @@ O que deseja fazer:
             radio1.diminuir_volume(valor)
             continue
         
-        ### MUDAR ESTAÇÃO
+        elif resposta == '6':
+            valor = int(input(
+'''
+'Estações disponíveis:
+
+[0] - Proxima estação              
+[1] - 89.5: 'Cocais
+[2] - 91.5: 'Mix'
+[3] - 94.1: 'Boa'
+[4] - 99.1: 'Clube'
+>
+'''))
+            radio1.mudar_frequencia(valor)
+            continue
         
         elif resposta == '7':
-            print(radio1.definicao)
+            print('Frequencia:', radio1.frequencia)
+            print('Estação:', radio1.estacao)
             continue
         
         elif resposta == '8':
+            print(radio1.definicao)
+            continue
+        
+        elif resposta == '9':
             print('Encerrando programa...')
             break
         
