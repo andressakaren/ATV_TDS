567700dc329db8989328ae37c1bb400acd421955
andressakaren <160805915+andressakaren@users.noreply.github.com>
2025-02-12 11:36:10 -0300

    update lista7.sql

--- BD/Lista07.sql
+++ BD/Lista07.sql
@@ -26,7 +26,7 @@ WHERE	d.nome = 'Matemática'
 	AND d.disciplina_id = m.disciplina_id
 	AND m.aluno_id = a.aluno_id
 ORDER BY a.nome;
-
+	
 -- Listar, para todas as disciplinas, o nome da disciplina e o nome do aluno com média maior
 -- ou igual a 7 naquela disciplina, com agrupamento por aluno.
 
@@ -35,3 +35,49 @@ FROM	aluno a, matricula m, disciplina d
 WHERE	m.media >= 7 
 	AND d.disciplina_id = m.disciplina_id
 	AND m.aluno_id = a.aluno_id;
+--------------------------------------------------------------------------------------------
+-- QUESTÃO 01 --
+-- Exibir todas as disciplinas com suas respectivas cargas horárias
+SELECT 	d.nome, d.carga_horaria
+FROM 	disciplina d
+-- Total rows: 5
+
+-- QUESTÃO 02 --
+-- Exibir nome das disciplinas com carga horária inferior a 60 horas
+SELECT 	d.nome, d.carga_horaria
+FROM 	disciplina d
+WHERE	d.carga_horaria < 60;
+-- Total rows: 1
+
+-- QUESTÃO 03 --
+-- Exibir todos os alunos matriculados na disciplina 'Matemática' ordenados por nome.
+SELECT 	a.nome
+FROM 	aluno a, matricula m, disciplina d
+WHERE	d.nome = 'Matemática' AND
+		d.disciplina_id = m.disciplina_id AND
+		m.aluno_id = a.aluno_id
+ORDER BY a.nome;
+-- Total rows: 18
+
+-- QUESTÃO 04 --
+-- Exibir a quantidade de faltas do aluno de id = 16 na disciplina 'Ciências'
+
+SELECT	m.faltas, a.nome
+FROM 	aluno a, matricula m, disciplina d
+WHERE 	a.aluno_id = 16 AND
+		a.aluno_id = m.aluno_id AND
+		m.disciplina_id = d.disciplina_id AND
+		d.nome = 'Ciências';
+-- Total: 8 faltas
+
+-- QUESTÃO 05 -- 
+-- Exibir todos os alunos com média menor ou igual a 8 na disciplina 'História'
+
+SELECT	a.nome
+FROM	aluno a, matricula m, disciplina d
+WHERE	m.media <= 8 AND 
+		d.nome = 'História' AND
+		a.aluno_id = m.aluno_id AND
+		m.disciplina_id = d.disciplina_id
+-- Total rows: 11
+		
