ee025c9cfe500e2b4c5621fc6b58576ed0b34a1f
Andressa Karen <lqsandressa@gmail.com>
2025-04-05 23:22:00 -0300

    correcao for para while

--- DOT/lista02/q1.py
+++ DOT/lista02/q1.py
@@ -1,7 +1,5 @@
 def par_impar(n):
-    if n % 2 == 0:
-        return True
-    return False
+    return n % 2 == 0
 
 
 def receber_inteiros(n):
@@ -9,9 +7,10 @@ def receber_inteiros(n):
     lista_par = []
     lista_impar = []
 
-    for i in range(n):
+    count = 0
+    while count < n:
         try:
-            valor = int(input('Digite um numero inteiro: '))
+            valor = int(input(f'Digite o {count + 1}° numero inteiro: '))
             lista_total.append(valor)
 
             if par_impar(valor):
@@ -20,20 +19,21 @@ def receber_inteiros(n):
             else:
                 if valor not in lista_impar:
                     lista_impar.append(valor)
+            count += 1
         except:
             print('Digite um número válido!')
     return lista_total, lista_par, lista_impar
 
 
-lista_total, lista_par, lista_impar = receber_inteiros(100)
+lista_total, lista_par, lista_impar = receber_inteiros(5)
 
 # 1) Faça um programa que grave uma lista de 100 elementos numéricos inteiros e:
-print('Lista total: ', lista_total)
+print('Lista total:', lista_total)
 # a) Mostre a quantidade de números pares;
-print('Quantidade de números pares: ', len(lista_par))
+print('Quantidade de números pares:', len(lista_par))
 # b) Grave uma lista somente com os números pares e mostre a lista;
-print('Lista somente com números pares: ', lista_par)
+print('Lista somente com números pares:', lista_par)
 # c) Mostre a quantidade de números ímpares;
-print('Quantidade de números impares: ', len(lista_impar))
+print('Quantidade de números impares:', len(lista_impar))
 # d) Grave uma lista somente com os números ímpares e mostre a lista.
-print('Lista somente com números impares: ', lista_impar)
+print('Lista somente com números impares:', lista_impar)
