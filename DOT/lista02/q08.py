c5a18c9936b1dd39f104b1c8d199c383f6dfb3a6
Andressa Karen <lqsandressa@gmail.com>
2025-04-08 18:15:30 -0300

    q8

---
+++ DOT/lista02/q08.py
@@ -0,0 +1,25 @@
+# 8) Dada uma lista contendo letras do alfabeto, elabore um programa para verificar quantas vezes
+# ocorreu a letra ‘A’.
+# OBS: Fazer crítica na entrada do caractere para aceitar somente letras.
+
+
+def lista_letras(n):
+    lista = []
+    count = 0
+    total = 0
+    while count < n:
+        try:
+            valor = str(
+                input(f"Digite a {count+1}° letra da lista: ")).strip().upper()
+            lista.append(valor)
+            count += 1
+            if valor == 'A':
+                total += 1
+        except ValueError:
+            print("Valor inválido. Tente novamente.")
+    return lista, total
+
+
+lista, total = lista_letras(5)
+print('A lista:', lista)
+print('A quantidade de vezes que a letra "A" foi inserida:', total)
