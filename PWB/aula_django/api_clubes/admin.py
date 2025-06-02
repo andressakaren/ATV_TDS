dbef4571fac85fb111b82cc1aa79c8783fc97355
Andressa Karen <lqsandressa@gmail.com>
2025-06-02 20:36:14 -0300

    algumas coisas diferente da aula mas dps mudar pro codigo github

--- PWB/aula_django/api_clubes/admin.py
+++ PWB/aula_django/api_clubes/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from api_clubes.models import Clube, Jogador
 
 # Register your models here.
+
+admin.site.register(Clube)
+admin.site.register(Jogador)
--- PWB/aula_django/api_clubes/models.py
+++ PWB/aula_django/api_clubes/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Clube(models.Model):
+    nome_time = models.CharField(max_length=50)
+    
+    def __str__(self) -> str:
+        return self.nome_time
+
+class Jogador(models.Model):
+    nome_jogador = models.CharField(max_length=50)
+    clube = models.ForeignKey(
+        Clube,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
+    def __str__(self) -> str:
+        return self.nome_jogador
+    
