df1a85596df3771b896af0f053043ee05c69c767
Andressa Karen <lqsandressa@gmail.com>
2025-06-02 19:51:45 -0300

    mudar titulo do painel admin

--- PWB/aula_django/project/urls.py
+++ PWB/aula_django/project/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+
+admin.site.site_header = "Aula 1 django - cadastro de times "
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
